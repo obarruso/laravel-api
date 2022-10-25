4e1e45b803488b5a588da2cb894e5fc64a97ef2c
Oier Barruso <oerbarruso@gmail.com>
2022-10-25 19:15:51 +0200

    Save data with post

--- code/app/Http/Controllers/Api/V1/CustomerController.php
+++ code/app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\CustomerFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
@@ -55,7 +55,7 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
--- code/app/Models/Customer.php
+++ code/app/Models/Customer.php
@@ -9,7 +9,16 @@ class Customer extends Model
 {
     use HasFactory;
 
-    // protected $fillable =
+    protected $fillable = [
+        'id',
+        'name',
+        'type',
+        'email',
+        'address',
+        'city',
+        'state',
+        'postal_code',
+    ];
 
     public function invoices() {
         return $this->hasMany(Invoice::class);
