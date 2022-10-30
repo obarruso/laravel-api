2cab4bca7fa1b90d42b6810fd865381fd90d3a08
Oier Barruso <oerbarruso@gmail.com>
2022-10-30 13:43:00 +0100

    Make tokens for protecting routes

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\V1\CustomerController;
-use App\Http\Controllers\Api\V1\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
 
--- code/routes/web.php
+++ code/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/setup', function()
+{
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+    if (!Auth::attempt($credentials)) {
+        echo 'hola';
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+    }
+
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+
+        $adminToken = $user->createToken('admin-token', ['create','update','delete']);
+        $updateToken = $user->createToken('update-token', ['create','update']); // invoice:create
+        $basicToken = $user->createToken('basic-token',  ['none']);
+
+        return [
+            'admin' => $adminToken->plainTextToken,
+            'update' => $updateToken->plainTextToken,
+            'basic' => $basicToken->plainTextToken,
+        ];
+    }
 });
